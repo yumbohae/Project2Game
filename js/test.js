00863c9cc184fb107b4c6daf9944e8da8308b986
yumbohae <paolocarino33@gmail.com>
2022-11-10 08:11:10 -0600

    a self-reflective game

---
+++ js/test.js
@@ -0,0 +1,21 @@
+
+function drink() 
+    {
+    document.getElementById("demo").innerHTML = "lovely! let's continue";
+    document.getElementById("demo").style.color = "#003b00";
+    
+    document.getElementById("drink").style.color = "white";
+    document.getElementById("drink").style.backgroundColor = "green";
+    }
+
+function toggle(){
+    let qyy = document.getElementById("questiony");
+    let qyn = document.getElementById("questionn")
+    let vis = document.getElementById("caption");
+    qyn.style.visibility = 'visible';
+    qyy.style.visibility = 'visible';
+    vis.style.visibility = 'visible';
+
+
+
+}
